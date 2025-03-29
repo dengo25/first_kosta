68dceabbb9c55ad05ac31aaf8cfa68f0acb853ac
dengo25 <dengo25.dev@gmail.com>
2025-03-29 15:22:34 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import common.RunMenu;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        RunMenu runMenu = new RunMenu();
+
+        runMenu.run();
+    }
+}
